9f6f3bd96d37eddbdf4b55696dc6444ec848c33e
Snorlax <radovanne3@gmail.com>
2021-12-01 20:32:45 +0100

    first commit

---
+++ test/snorlax/AdventOfCode2021_test.clj
@@ -0,0 +1,7 @@
+(ns snorlax.AdventOfCode2021-test
+  (:require [clojure.test :refer :all]
+            [snorlax.AdventOfCode2021 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
